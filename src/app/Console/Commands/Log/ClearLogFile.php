c671c33e489fab10875f6ed342d516c05b4f5041
YurchenkoAndrew <webmaster@wsa.kz>
2023-12-17 18:11:56 +0600

    Добавил регистрацию и Авторизацию пользователей.

---
+++ src/app/Console/Commands/Log/ClearLogFile.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands\Log;
+
+use Illuminate\Console\Command;
+
+class ClearLogFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+//    php artisan log:clear
+
+    protected $signature = 'log:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear Laravel log';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        exec('echo "" > ' . storage_path('logs/laravel.log'));
+        $this->info('Logs have been cleared');
+        return true;
+    }
+}
